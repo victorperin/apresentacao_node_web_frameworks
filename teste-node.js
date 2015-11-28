af9d37ec2f56049122e1ed7ef235e9f25717d125
Victor Perin <me@victorperin.com>
2015-11-28 01:08:10 -0200

    corrigindo código sem framework

--- teste-node.js
+++ teste-node.js
@@ -1,19 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 
+console.log('Servidor ouvindo na porta 1337!');
 http.createServer(function(request, response) {
-  if (request.url == "/") {
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    fs.createReadStream("./index.html").pipe(response);
+  if (request.url == '/') {
+    response.writeHeader(200, {'Content-Type': 'text/html'});
+    fs.createReadStream('./index.html').pipe(response);
   } else {
-    fs.stat("."+request.url, function (err,status){
-      if(!err && status.isFile()){
+    fs.stat('.' + request.url, function(err, status) {
+      if (!err && status.isFile()) {
         response.writeHeader(200);
-        fs.createReadStream("."+request.url).pipe(response);
-      }
-      else{
+        fs.createReadStream('.' + request.url).pipe(response);
+      } else {
         response.writeHeader(404);
-        response.end("Arquivo nao encontrado.")
+        response.end('Arquivo nao encontrado.');
       }
     });
   }
